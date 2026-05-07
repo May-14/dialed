440d16e577380a5ef5b4a4a5f817c20c6179a305
Matias <26matmoran@gmail.com>
2026-05-06 18:24:45 -0600

    simplify data collection method

--- script/brew_script.js
+++ script/brew_script.js
@@ -1,30 +1,12 @@
 const data = JSON.parse(localStorage.getItem("dialedData"))
 
+const { brewSettings, coffeeInfo, waterComposition, brewPreferences } = data
 
-// <div class="brew-settings">
-//     <h3>Brew settings</h3>
-// </div>
-// <div class="coffee-info">
-//     <h3>Coffee info</h3>
-// </div>
-// <div class="water-composition">
-//     <h3>Water composition</h3>
-// </div>
-// <div class="brew-preferences">
-//     <h3>Brew preferences</h3>
-// </div>
+console.log(brewSettings)
+console.log(coffeeInfo)
+console.log(waterComposition)
+console.log(brewPreferences)
 
-let brewSettingsData = [String, String]
-
-let i = 0
-for (key in data) {
-    if (i < 4) {
-        brewSettingsData[key] = data[key]
-    }
-    i+=1
-}
-
-console.log(brewSettingsData)
 
 
 
