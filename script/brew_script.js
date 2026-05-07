608440bc9409c0e2902ff03634aad1f0c6e18f1f
Matias <26matmoran@gmail.com>
2026-05-06 18:24:58 -0600

    simplify data collection method

--- script/brew_script.js
+++ script/brew_script.js
@@ -2,10 +2,7 @@ const data = JSON.parse(localStorage.getItem("dialedData"))
 
 const { brewSettings, coffeeInfo, waterComposition, brewPreferences } = data
 
-console.log(brewSettings)
-console.log(coffeeInfo)
-console.log(waterComposition)
-console.log(brewPreferences)
+
 
 
 
